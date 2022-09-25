6635c6a1663752357f8eaf8a774add1cdebc35e4
xiuqizhiping1034 <1494360481@qq.com>
2022-09-25 20:52:45 +0800

    默认的

--- 1HelloWorld/hello.py
+++ 1HelloWorld/hello.py
@@ -2,4 +2,12 @@ print('hello world')
 print('hello world')
 print('hello world')
 print('hello world')
-
+"""测试使用"""
+print('hello world')
+print('hello world')
+print('hello world')
+print('hello world')
+print('hello world')
+print('hello world')
+print('hello world')
+print('hello world')
